1166011cb221b6077e428e02509eccdfb184549a
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2021-10-12 15:42:36 -0300

    update

--- helio-website/pages/index.tsx
+++ helio-website/pages/index.tsx
@@ -1,41 +1,89 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import React from 'react';
+import { Link } from 'react-scroll';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Grupo Helio</title>
-        <meta name="description" content="Helio Group homepage" />
+        <meta name="description" content="Helio homepage" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        {/* <h1 className={styles.title}>
-          Helio Group
-        </h1> */}
+      <div className={styles.panel} id="section1">
+        <img src="/logo_transparent_background_resized.png" className={styles.logo}/>
+        <div className={styles.arrow_containter}>
+          <img src="/keyboard_double_arrow_down_black_24dp.svg"/>
+          <Link
+            activeClass="active"
+            to="section2"
+            spy={true}
+            smooth={true}
+            duration={500}
+            ignoreCancelEvents={true}
+          >
+            <div></div>
+          </Link>
+        </div>
+      </div>
 
-        <Image src="/logo_transparent_background_resized.png" alt="Vercel Logo" width={418} height={157} />
+      <div className={styles.panel} id="section2">
+        <div className={styles.arrow_containter}>
+          <img src="/keyboard_double_arrow_up_black_24dp.svg"/>
+          <Link
+            activeClass="active"
+            to="section1"
+            spy={true}
+            smooth={true}
+            duration={500}
+            ignoreCancelEvents={true}
+          >
+            <div></div>
+          </Link>
+        </div>
+        <h1>Light methodologies, noble development,<br />and always going up!</h1>
+        <div className={styles.arrow_containter}>
+          <img src="/keyboard_double_arrow_down_black_24dp.svg"/>
+          <Link
+            activeClass="active"
+            to="section3"
+            spy={true}
+            smooth={true}
+            duration={500}
+            ignoreCancelEvents={true}
+          >
+            <div></div>
+          </Link>
+        </div>
+      </div>
 
-        <Image src="/keyboard_double_arrow_down_black_24dp.svg" alt="Something"  width={96} height={96} />
-
-        <h1>Light methodologies, noble development,<br />
-          and always going up!</h1>
-
+      <div className={styles.panel} id="section3">
+        <div className={styles.arrow_containter}>
+          <img src="/keyboard_double_arrow_up_black_24dp.svg"/>
+          <Link
+            activeClass="active"
+            to="section2"
+            spy={true}
+            smooth={true}
+            duration={500}
+            ignoreCancelEvents={true}
+          >
+            <div></div>
+          </Link>
+        </div>
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Challenge us! &rarr;</h2>
             <p>A solution you need? An idea you want to test? Just let us know!</p>
           </a>
-
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Our achievements &rarr;</h2>
             <p>Not sure about us? Let our results<br />
               and experience talk for us.</p>
           </a>
-
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
@@ -43,7 +91,6 @@ const Home: NextPage = () => {
             <h2>What do we do? &rarr;</h2>
             <p>We get things done well, or we die trying. We never died, so it has been a success until now.</p>
           </a>
-
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
@@ -54,20 +101,7 @@ const Home: NextPage = () => {
             </p>
           </a>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      </div>
     </div>
   )
 }
